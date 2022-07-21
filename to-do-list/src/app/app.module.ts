247902ecac6925187f118f75c9a935e7bcef2cde
FrasMaciej <maciejfrs@gmail.com>
2022-07-21 09:58:57 +0200

    replacement top-bar with material angular component

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { AddTaskFieldComponent } from './add-task-field/add-task-field.component';
 import { TasksSummaryComponent } from './tasks-summary/tasks-summary.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,12 +22,15 @@ import { TasksSummaryComponent } from './tasks-summary/tasks-summary.component';
     TopBarComponent,
     TasksListComponent,
     AddTaskFieldComponent,
-    TasksSummaryComponent
+    TasksSummaryComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
